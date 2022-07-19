50d85858d76e1327f474b2eed49bf30c8c225954
Atomic Potato <55362397+Atomic-Potato@users.noreply.github.com>
2022-07-19 12:21:06 +0300

    Binary_Trees_Static.h

---
+++ header files/Binary_Trees_Static.h
@@ -0,0 +1,6 @@
+#include "Binary_Tree_Static_Type.h"
+
+extern void Create_Btree(Btree* B);
+
+extern int add_left_child(Btree* B, element parent, element left_child);
+extern int add_right_child(Btree* B, element parent, element left_child);
